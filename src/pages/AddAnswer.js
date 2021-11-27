06d19cfe9ad2bb22b8ad843de11e91fc490d7d96
Kev <kevin.heldner@epfl.ch>
2021-11-27 10:46:40 +0100

    Box  de question

--- src/pages/AddAnswer.js
+++ src/pages/AddAnswer.js
@@ -1,8 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class AddAnswer extends Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = { value: 'Réfiger votre question, ici.' };
+    }
+    handleChange(e) {
+        this.setState({ value: e.target.value });
+    }
+
+
     render() {
-        return <div></div>
+        return (
+            <div className="mb-3">
+                <h1 class="display-3">Question</h1>
+                <label for="Textarea" class="form-label">
+                    Comment avez-vous compris notre manière de noter?
+                </label>
+                <br />
+                <textarea
+                    class="form-control"
+                    id="Textarea"
+                    rows="7"
+                    onChange={this.handleChange}
+                    defaultValue={this.state.value}
+                />
+
+            </div>
+        );
     }
 }
 
